41ef0569584e656b392a22e2914db79dd0eadf4b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-13 09:31:54 +0900

    [add] - ユーザーページの表示と変数周り改善

--- src/routes/user/[id]/+page.svelte
+++ src/routes/user/[id]/+page.svelte
@@ -3,21 +3,26 @@
   import profileUpdate from "$lib/wsHandler/User/profileUpdate.ws";
   import { page } from "$app/stores";
   import { onMount } from "svelte";
+  import { get } from "svelte/store";
 
   let name = "";
-  let description = "";
+  let selfIntroduction = "";
 
   //ユーザー情報を更新させる
   const updateIt = () => {
     profileUpdate({
       name,
-      description,
+      selfIntroduction,
     });
   };
 
   onMount(() => {
     const userId = $page.params.id;
     console.log(userId);
+
+    //編集用にユーザー情報を取得
+    selfIntroduction = get(myUserStore).selfIntroduction;
+    name = get(myUserStore).name;
   });
 </script>
 
@@ -25,5 +30,19 @@
   <h1>ユーザー</h1>
   <p>id: {$page.params.id}</p>
 
-  <button on:click={updateIt} class="btn">更新</button>
+  <div>
+    <div class="card-body">
+      <textarea
+        bind:value={selfIntroduction}
+        class="textarea card bg-base-200"
+        placeholder="自己紹介文"
+      ></textarea>
+    </div>
+  </div>
+
+  <button
+    class="btn"
+    disabled={selfIntroduction === get(myUserStore).selfIntroduction &&
+      name === get(myUserStore).name}>更新</button
+  >
 </div>
