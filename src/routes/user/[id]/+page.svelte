ae5068d7f0656e7a0dafb3b4b9addad102c41842
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-13 09:03:33 +0900

    [add] - プロフィールボタンクリックしたらユーザーページへ飛ぶように

---
+++ src/routes/user/[id]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { myUserStore } from "$lib/store/myuser";
+  import profileUpdate from "$lib/wsHandler/User/profileUpdate.ws";
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+
+  let name = "";
+  let description = "";
+
+  //ユーザー情報を更新させる
+  const updateIt = () => {
+    profileUpdate({
+      name,
+      description,
+    });
+  };
+
+  onMount(() => {
+    const userId = $page.params.id;
+    console.log(userId);
+  });
+</script>
+
+<div>
+  <h1>ユーザー</h1>
+  <p>id: {$page.params.id}</p>
+
+  <button on:click={updateIt}>更新</button>
+</div>
