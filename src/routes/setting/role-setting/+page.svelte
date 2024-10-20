6c1575a384062350ecadbba98e4c3e6a8f49c845
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-20 17:55:14 +0000

    [add] - ロール管理ページの土台作成

--- src/routes/setting/role-setting/+page.svelte
+++ src/routes/setting/role-setting/+page.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import {
+    IconSettings,
+  } from "@tabler/icons-svelte";
 </script>
 
-<div></div>
+<div class="p-2 flex flex-col gap-3 mx-auto max-w-[950px]">
+  <div class="card bg-base-200 shadow-xl">
+    <div class="card-body flex flex-row">
+      <button
+        class="btn btn-ghost w-16"
+        on:click={() => {
+          goto("/setting");
+        }}
+      >
+        <IconSettings size={20} />
+      </button>
+      <h2 class="card-title">ロールの管理</h2>
+    </div>
+  </div>
+</div>
