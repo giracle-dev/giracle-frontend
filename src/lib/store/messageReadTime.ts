0f000cf7ed81c5660a733f1fdaa643da12f2a58b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-22 07:20:02 +0900

    [add] - 新着有無Storeの追加

--- src/lib/store/messageReadTime.ts
+++ src/lib/store/messageReadTime.ts
@@ -1,9 +1,16 @@
-import type { IChannel, IChannelHistory } from "$lib/types/IChannel";
+import type { IChannel } from "$lib/types/IChannel";
 import type { IMessage } from "$lib/types/IMessage";
 import { writable } from "svelte/store";
 
+//初期値
 const initialMessageReadTime: {[key: string]: Date} = {};
+const initialHasNewMessage: {[key: IChannel["id"]]: boolean} = {};
 
+//履歴の既読時間Store
 export const MessageReadTimeStore = writable<{[key: string]: Date}>(
   initialMessageReadTime,
+);
+//新着メッセージの有無Store
+export const hasNewMessageStore = writable<{[key: IChannel["id"]]: boolean}>(
+  initialHasNewMessage,
 );
