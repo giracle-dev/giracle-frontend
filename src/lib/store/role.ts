ebb5f46efff34cf78b53e56c6516eec378c34775
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-25 17:17:17 +0900

    [change] - ロール取得関数にて確実にIRoleのデータを返せるように

--- src/lib/store/role.ts
+++ src/lib/store/role.ts
@@ -27,21 +27,15 @@ export const roleStore = writable<{[key: string]: IRole}>(initialRole);
 export const getRoleInfo = async (id: string): Promise<IRole> => {
   if (get(roleStore)[id]) return get(roleStore)[id];
 
+  console.log("store(role) :: getRoleInfo : Storeにないので取得します roleId->", id);
+
   //ロールがなければ取得してから返す
-  await roleRepository
-    .getRoleInfo(id)
-    .then((res) => {
-      roleStore.update((roles) => {
-        roles[id] = res.data;
-        return roles;
-      });
-      return get(roleStore)[id];
-    })
-    .catch(() => {
-      return {
-        id: "ERROR",
-        name: "エラー",
-        color: "#f00",
+  try {
+    roleStore.update((roles) => {
+      roles[id] = {
+        id: id,
+        name: "Loading...",
+        color: "#fff",
         createdAt: new Date(),
         createdUserId: "SYSTEM",
         manageUser: false,
@@ -49,17 +43,25 @@ export const getRoleInfo = async (id: string): Promise<IRole> => {
         manageRole: false,
         manageServer: false,
       };
+      return roles;
     });
-
-  return {
-    id: "ERROR",
-    name: "エラー",
-    color: "#f00",
-    createdAt: new Date(),
-    createdUserId: "SYSTEM",
-    manageUser: false,
-    manageChannel: false,
-    manageRole: false,
-    manageServer: false,
-  };
+    const res = await roleRepository.getRoleInfo(id);
+    roleStore.update((roles) => {
+      roles[id] = res.data;
+      return roles;
+    });
+    return get(roleStore)[id];
+  } catch {
+    return {
+      id: "ERROR",
+      name: "エラー",
+      color: "#f00",
+      createdAt: new Date(),
+      createdUserId: "SYSTEM",
+      manageUser: false,
+      manageChannel: false,
+      manageRole: false,
+      manageServer: false,
+    };
+  }
 }
