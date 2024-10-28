670dea41773859ef1d4e0981bd956516eb14bcc5
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-28 18:30:41 +0900

    [add] - ファイル添付表示用のコンポーネント作成

---
+++ src/lib/components/channel/Messageinput/FileChip.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let fileData: File;
+</script>
+
+<div class="card bg-base-200 px-3 py-2">
+  {#if fileData.type.startsWith("image/")}
+    <figure>
+      <img
+        src={URL.createObjectURL(fileData)}
+        alt={fileData.name}
+        class="max-h-[150px] max-w-[150px]"
+      />
+    </figure>
+  {/if}
+
+  <p>
+    {fileData.name.length > 15
+      ? fileData.name.slice(0, 15) + "..."
+      : fileData.name}
+  </p>
+</div>
