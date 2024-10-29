2fc544012af327bbc17d8666247af6b79fcaa1f0
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-29 17:15:51 +0900

    [add] - ストレージ管理ページのコンポ作成と移動ボタン作成

---
+++ src/routes/setting/storage-manage/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+import {goto} from "$app/navigation";
+import {IconSettings} from "@tabler/icons-svelte";
+</script>
+
+<div class="p-2 mx-auto max-w-[850px] flex flex-col gap-3">
+  <div class="card bg-base-200 p-3 flex flex-row items-center">
+    <button
+            class="btn btn-ghost w-16"
+            on:click={() => {
+        goto("/setting");
+      }}
+    >
+      <IconSettings size={20} />
+    </button>
+    <p class="card-title">ストレージ管理</p>
+  </div>
+</div>
