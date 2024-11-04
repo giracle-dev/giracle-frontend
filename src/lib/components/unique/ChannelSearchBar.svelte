6ece1cf49add218882bdaad010d2965fa60b425b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-04 19:51:39 +0900

    [add] - チャンネル検索用コンポーネントの土台作成

---
+++ src/lib/components/unique/ChannelSearchBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { repositoryFactory } from "$lib/repositories/RepositoryFactory";
+  import type { IChannel } from "$lib/types/IChannel";
+  const channelRepository = repositoryFactory.get("channel");
+
+  let result: IChannel[] = []; //検索結果格納用
+  let selection: IChannel[] = []; //結果から選択されたチャンネルを格納
+  let query = ""; //検索クエリ
+
+  //チャンネルを検索する
+  const searchChannel = () => {
+
+  }
+</script>
+
+<div>
+  <input type="text" placeholder="Search for a channel" />
+  <button>Search</button>
+</div>
