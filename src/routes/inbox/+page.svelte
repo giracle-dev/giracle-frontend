fe3c87a125d26a66197e1e1706510027197607c7
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-11 03:21:37 +0900

    [add] - インボックスからチャンネルでの通知を丸ごと既読にする関数作成

--- src/routes/inbox/+page.svelte
+++ src/routes/inbox/+page.svelte
@@ -17,8 +17,26 @@
   } from "@tabler/icons-svelte";
   import type { IChannel } from "$lib/types/IChannel";
   import CompactMessageRender from "$lib/components/unique/CompactMessageRender.svelte";
+  import { ReadInboxItem } from "$lib/utils/ReadInboxItem";
+  import { get } from "svelte/store";
 
-  let groupByChannel: boolean = false;
+  let groupByChannel: boolean = true;
+  let processing = false;
+
+  /**
+   * 指定のチャンネルの通知を全て既読にする
+   * @param channelId
+   */
+  const readWholeChannel = async (channelId: string) => {
+    processing = true;
+    const items = get(inboxStore).filter(
+      (item) => item.Message.channelId === channelId,
+    );
+    for (const item of items) {
+      await ReadInboxItem(item.Message.id);
+    }
+    processing = false;
+  };
 </script>
 
 <div class="container px-2 pt-2 h-full max-w-[1150px] mx-auto overflow-y-auto">
@@ -36,17 +54,25 @@
   {#if groupByChannel}
     {#each $channelListStore as channel}
       {#if $inboxStore.some((item) => item.Message.channelId === channel.id)}
-        <a
-          href="/channel/{channel.id}"
-          class="flex items-center gap-2 p-2 hover:bg-base-100"
-        >
-          <div class="text-xl mb-2 text-gray-500"># {channel.name}</div>
-        </a>
+        <div class="flex items-center">
+          <div class="text-xl truncate mb-2 text-gray-500">
+            <a href="/channel/{channel.id}" class="flex items-center gap-2 p-2">
+              # {channel.name}
+            </a>
+          </div>
+          <button
+            on:click={() => readWholeChannel(channel.id)}
+            class="ml-auto btn btn-primary btn-sm"
+            disabled={processing}>既読する</button
+          >
+        </div>
+
         <div class="flex flex-col gap-1">
           {#each $inboxStore.filter((item) => item.Message.channelId === channel.id) as inboxItem, index}
             <CompactMessageRender message={inboxItem.Message} />
           {/each}
         </div>
+
         <div class="divider" />
       {/if}
     {/each}
