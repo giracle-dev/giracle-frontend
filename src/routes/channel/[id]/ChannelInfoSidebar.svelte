267f9f267d115b9e7c73cfd450870828e879b8c3
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-17 05:04:25 +0900

    [add] - チャンネルサイドバーコンポの土台作成と埋め込み

---
+++ src/routes/channel/[id]/ChannelInfoSidebar.svelte
@@ -0,0 +1,20 @@
+<div class="drawer drawer-end lg:drawer-open">
+  <input id="channel-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col items-center justify-center">
+    <!-- Page content here -->
+    <label for="channel-drawer" class="btn btn-primary drawer-button">
+      Open drawer
+    </label>
+  </div>
+  <div class="drawer-side">
+    <label
+      for="channel-drawer"
+      aria-label="close sidebar"
+      class="drawer-overlay"
+    ></label>
+    <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+      <!-- Sidebar content here -->
+      <p>ここに内容</p>
+    </ul>
+  </div>
+</div>
