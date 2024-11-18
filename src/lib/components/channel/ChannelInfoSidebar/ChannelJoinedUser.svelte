b0ecf74b9cc0821543b66f604e21e75a921d02be
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-18 12:33:31 +0900

    [add] - チャンネル参加者を簡易的に表示できるように

--- src/lib/components/channel/ChannelInfoSidebar/ChannelJoinedUser.svelte
+++ src/lib/components/channel/ChannelInfoSidebar/ChannelJoinedUser.svelte
@@ -1,1 +1,23 @@
-<div>チャンネル参加者リスト</div>
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { userListStore } from "$lib/store/user";
+  import type { IUser } from "$lib/types/IUser";
+  import { onMount } from "svelte";
+
+  let users: IUser[] = [];
+
+  const getJoinedUser = () => {
+    users = $userListStore.filter((user) =>
+      user.ChannelJoin.some((c) => c.channelId === $page.params.id),
+    );
+  };
+
+  onMount(() => getJoinedUser());
+</script>
+
+<div>
+  チャンネル参加者リスト
+  {#each users as user}
+    <p>{user.name}</p>
+  {/each}
+</div>
