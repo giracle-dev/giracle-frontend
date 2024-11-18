d84f8a519f025ae165dc74c3a2d84c4fc3d7a53c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-18 11:31:35 +0900

    [add] - サイドバー用のコンポの土台作成と表示切替ボタン追加

--- src/routes/channel/[id]/ChannelInfoSidebar.svelte
+++ src/routes/channel/[id]/ChannelInfoSidebar.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+  import ChannelJoinedUser from "$lib/components/channel/ChannelInfoSidebar/ChannelJoinedUser.svelte";
+  import { page } from "$app/stores";
+  import { channelListStore } from "$lib/store/channel";
+  import { get } from "svelte/store";
+
+  let displayMode: "JOINED_USERS" | "MANAGE_PANEL" = "JOINED_USERS";
+</script>
+
 <div class="absolute drawer drawer-end lg:drawer-open">
   <input id="channel-drawer" type="checkbox" class="drawer-toggle" />
   <!--
@@ -14,8 +23,34 @@
       class="drawer-overlay"
     ></label>
     <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-      <!-- Sidebar content here -->
-      <p>ここに内容</p>
+      <div class="flex flex-col h-full gap-4">
+        <!-- Sidebar content here -->
+        <div class="card truncate bg-base-100 w-full py-3 px-4">
+          <div class="card-title">
+            # {get(channelListStore).find((c) => c.id === $page.params.id)
+              ?.name}
+          </div>
+        </div>
+
+        <div class="join mx-auto">
+          <button
+            on:click={() => (displayMode = "JOINED_USERS")}
+            class={`btn join-item ${displayMode === "JOINED_USERS" ? "btn-primary" : ""}`}
+            >参加者</button
+          >
+          <button
+            on:click={() => (displayMode = "MANAGE_PANEL")}
+            class={`btn join-item ${displayMode === "MANAGE_PANEL" ? "btn-primary" : ""}`}
+            >管理</button
+          >
+        </div>
+
+        <div class="card bg-base-100 py-3 px-4">
+          {#if displayMode === "JOINED_USERS"}
+            <ChannelJoinedUser />
+          {/if}
+        </div>
+      </div>
     </ul>
   </div>
 </div>
