e6b3a2d8de78d4630b9ff7c95218fccb52615834
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-21 10:20:50 +0900

    [add] - WS接続時にオンラインユーザーを同期するように

--- src/lib/wsHandler/INIT.ws.ts
+++ src/lib/wsHandler/INIT.ws.ts
@@ -7,6 +7,8 @@ import { deleteMessageWsOn } from "./message/deleteMessage";
 import { writable } from "svelte/store";
 import { inboxAddedWsOn } from "./message/inboxAdded";
 import { inboxDeletedWsOn } from "./message/inboxDelete";
+import { onlineUserListStore } from "$lib/store/user";
+import { repositoryFactory } from "$lib/repositories/RepositoryFactory";
 
 //WSインスタンス
 export let ws: WebSocket = new WebSocket("/ws");
@@ -28,6 +30,16 @@ export const initWS = () => {
     FLAGwsError = false;
     //接続状態を更新
     wsStatusStore.set(ws.readyState);
+
+    //オンラインユーザー情報取得
+    const userRepository = repositoryFactory.get("user");
+    userRepository.getOnline().then((response) => {
+      console.log(
+        "INIT.ws :: onmessage : response(getOnline)->",
+        response,
+      );
+      onlineUserListStore.set(response.data);
+    });
   };
 
   ws.onerror = (event) => {
