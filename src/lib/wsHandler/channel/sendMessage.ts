037e963b6490378c161874c74797a8e94a302ca5
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-25 09:00:04 +0900

    Notification (#80)
    * [add] - 通知を出すように
    
    * [add] - 別タブにいたときの通知処理
    
    * [change] - 通知処理を関数化
    
    * [add] - Giracleアクセス時にブラウザに通知許可を求める
    
    * [add] - 自分宛てのメンションで通知するように
    
    * [add] - 通知のクリックでチャンネル移動をするように
    
    * [add] - WS接続時にオンラインユーザーを同期するように

--- src/lib/wsHandler/channel/sendMessage.ts
+++ src/lib/wsHandler/channel/sendMessage.ts
@@ -1,10 +1,13 @@
 import type { IMessage } from "$lib/types/IMessage";
 import { channelHistoryStore } from "$lib/store/channelHistory";
 import { hasNewMessageStore } from "$lib/store/messageReadTime";
+import { myUserStore, userListStore } from "$lib/store/user";
+import { channelListStore } from "$lib/store/channel";
 import { get } from "svelte/store";
 import { page } from "$app/stores";
 import updateReadTime from "$lib/utils/updateReadTime";
 import { ReadInboxItem } from "$lib/utils/ReadInboxItem";
+import { goto } from "$app/navigation";
 
 interface IResponseWsSendMessage {
   signal: "message::SendMessage";
@@ -23,6 +26,9 @@ window.addEventListener('blur', () => {
   // ウィンドウがフォーカスを失ったときの処理
   WINDOW_FOCUS = false;
 });
+
+//ブラウザ通知用インスタンス
+let NOTIFICATION_INSTNCE: Notification | null = null;
 
 export const sendMessageWsOn = async (data: IResponseWsSendMessage) => {
   if (data.signal === "message::SendMessage") {
@@ -45,6 +51,9 @@ export const sendMessageWsOn = async (data: IResponseWsSendMessage) => {
             [data.data.channelId]: true,
           }
         ));
+
+        //自分に対するメンション文があるなら通知
+        if (data.data.content.includes("@<" + get(myUserStore).id + ">")) notify(data.data);
       }
 
       //履歴Storeへ追加
@@ -62,6 +71,32 @@ export const sendMessageWsOn = async (data: IResponseWsSendMessage) => {
         }
       ));
       //console.log("hasNewMessageStore", get(hasNewMessageStore)[data.data.channelId]);
+
+      //自分に対するメンション文があるなら通知
+      if (data.data.content.includes("@<" + get(myUserStore).id + ">")) notify(data.data);
     }
   }
 };
+
+/**
+ * ブラウザ通知を出す
+ * @param msg 通知するメッセージデータ
+ */
+const notify = (msg: IMessage) => {
+  //通知が表示されていることを考慮して閉じておく
+  if (NOTIFICATION_INSTNCE) NOTIFICATION_INSTNCE.close();
+  //チャンネル名とユーザー名を取得
+  let channelName = get(channelListStore).find((channel) => channel.id === msg.channelId)?.name;
+  let sendersName = get(userListStore).find((user) => user.id === msg.userId)?.name;
+  
+  //通知を出す
+  NOTIFICATION_INSTNCE = new Notification("#" + channelName + " :: " + sendersName, {
+    body: msg.content,
+    icon: "/api/users/icon/" + msg.userId,
+    
+  });
+  //通知がクリックされたらチャンネルに飛ぶ
+  NOTIFICATION_INSTNCE.onclick = () => {
+    goto("/channel/" + msg.channelId);
+  };
+}
