753a7c1293a083dc606e5d667b72979c86ed2787
IriSValkyrie <59800473+iriSValkyie@users.noreply.github.com>
2024-12-05 00:22:30 +0900

    add:ImageViewer追加 #69 (#91)

--- src/routes/channel/[id]/FilePreview.svelte
+++ src/routes/channel/[id]/FilePreview.svelte
@@ -5,21 +5,34 @@
   export let fileData: IMessageFileAttached;
   export let forcedImageSize: string | undefined = "auto";
 
+  const filePath = "/api/message/file/";
+
+  let imagePreviewModal:HTMLDialogElement;
+
   //ファイルをダウンロードする
   const downloadFile = () => {
     const link = document.createElement("a");
-    link.href = `/api/message/file/${fileData.id}`;
+    link.href = `${filePath}${fileData.id}`;
     link.download = fileData.actualFileName;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
+
+  const previewImage = (fileData :IMessageFileAttached)=>{
+    if(!fileData.type.startsWith("image")) return;
+    console.log("clickedImage!");
+    imagePreviewModal.showModal();
+
+  }
+
 </script>
 
 <div class="h-fit">
+  <button  on:click={()=>previewImage(fileData)}>
   {#if fileData.type.startsWith("image")}
     <img
-      src={`/api/message/file/${fileData.id}`}
+      src={`${filePath}${fileData.id}`}
       alt={fileData.actualFileName}
       class={`h-${forcedImageSize ? "[" + forcedImageSize + "]" : "auto"} max-h-[150px] md:max-h-[350px] rounded-md`}
     />
@@ -33,4 +46,16 @@
       </button>
     </div>
   {/if}
+  </button>
 </div>
+
+<dialog id="imagePreviewModal" class="modal" bind:this={imagePreviewModal} >
+
+  <img
+          src={`${filePath}${fileData.id}`}
+          alt={fileData.actualFileName}
+          class="x-auto m-auto px-10 py-20 w-auto h-auto max-h-screen max-w-screen pointer-events-none"
+  />
+  <button class="w-full h-full absolute top-0 left-0 modal-backdrop" on:click={()=>imagePreviewModal.close()}/>
+</dialog>
+
