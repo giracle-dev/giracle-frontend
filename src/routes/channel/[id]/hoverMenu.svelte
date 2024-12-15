abb57047c58748040a31ac45e9c26de260a9c14c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-12-16 04:05:05 +0900

    [add] - 絵文字ピッカーの土台追加

--- src/routes/channel/[id]/hoverMenu.svelte
+++ src/routes/channel/[id]/hoverMenu.svelte
@@ -1,37 +1,42 @@
 <script lang="ts">
   import { IconDots } from "@tabler/icons-svelte";
-  import {toastStore} from "$lib/store/toast";
+  import { toastStore } from "$lib/store/toast";
+  import { Picker } from "emoji-mart";
+  import data from "@emoji-mart/data";
   export let messageId: string = "";
   export let hoverMessageId: string = "";
   export let isLast: boolean = false;
   import { repositoryFactory } from "$lib/repositories/RepositoryFactory";
+  import { onMount } from "svelte";
 
   const messageRepository = repositoryFactory.get("message");
   let isDisplayDeleteModal: boolean = false;
   let isHoverDropDown: boolean = false;
-  let deleteModal:HTMLDialogElement;
+  let deleteModal: HTMLDialogElement;
 
-  const deleteMessage = async (messageId :string)=>{
+  let picker = null;
+
+  const deleteMessage = async (messageId: string) => {
     console.log("deleting");
     await messageRepository
       .deleteMessage(messageId)
       .then((response) => {
-        console.log("メッセージ削除成功",response);
+        console.log("メッセージ削除成功", response);
         deleteModal.close();
-        toastStore.update((toast) =>{
+        toastStore.update((toast) => {
           return [
             ...toast,
             {
               message: "メッセージを削除しました。",
               type: "success",
-            }
-          ]
-        })
+            },
+          ];
+        });
       })
-      .catch((response) =>{
-        console.log("メッセージ削除失敗",response);
+      .catch((response) => {
+        console.log("メッセージ削除失敗", response);
         deleteModal.close();
-        toastStore.update((toast) =>{
+        toastStore.update((toast) => {
           return [
             ...toast,
             {
@@ -41,8 +46,21 @@
           ];
         });
       });
-  }
+  };
 
+  onMount(() => {
+    //絵文字リアクション用
+    picker = new Picker({
+      data: data,
+      emojiSize: 24,
+      title: "絵文字",
+      emoji: "point_up",
+      style: "native",
+      set: "apple",
+      showPreview: false,
+      showSkinTones: false,
+    });
+  });
 </script>
 
 <div class="relative">
@@ -77,21 +95,32 @@
         <li>
           <button
             class="text-error"
-            on:click={() =>{
-               console.log("OnClick Delete id:"+ hoverMessageId +"isDisplayDeleteModal:"+ isDisplayDeleteModal);
-               isDisplayDeleteModal = true;
-               deleteModal.showModal();
-            }}
-            >メッセージを削除</button
+            on:click={() => {
+              console.log(
+                "OnClick Delete id:" +
+                  hoverMessageId +
+                  "isDisplayDeleteModal:" +
+                  isDisplayDeleteModal,
+              );
+              isDisplayDeleteModal = true;
+              deleteModal.showModal();
+            }}>メッセージを削除</button
           >
         </li>
       </ul>
     </div>
   </div>
+
+  <!-- 絵文字リアクション用 -->
+  <div class="absolute -top-5 right-0" bind:this={picker} />
 </div>
 
 {#if isDisplayDeleteModal}
-  <dialog bind:this={deleteModal} id="deleteModal" class="modal modal-bottom sm:modal-middle">
+  <dialog
+    bind:this={deleteModal}
+    id="deleteModal"
+    class="modal modal-bottom sm:modal-middle"
+  >
     <div class="modal-box">
       <h3 class="text-lg font-bold">メッセージを削除</h3>
       <p class="py-4">メッセージを削除します。よろしいですか?</p>
@@ -100,7 +129,9 @@
           <!-- if there is a button in form, it will close the modal -->
           <button class="btn">キャンセル</button>
         </form>
-        <button class="btn" on:click={()=> deleteMessage(messageId)}>削除する</button>
+        <button class="btn" on:click={() => deleteMessage(messageId)}
+          >削除する</button
+        >
       </div>
     </div>
   </dialog>
